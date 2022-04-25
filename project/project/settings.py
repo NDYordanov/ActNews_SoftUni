8c5f789f62f0f3b0ef80bcf400da79cd3aa08568
Nikolay Yordanov <nikolaj2000@abv.bg>
2022-04-25 14:42:01 +0300

    Fixed Procfile

--- project/project/settings.py
+++ project/project/settings.py
@@ -6,7 +6,11 @@ SECRET_KEY = 'django-insecure-n64xhawv!!(h2)&@s&c!a%l-+w8jt56*i4f^2f0jnnr3sbdww#
 
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'act-news-project.herokuapp.com',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
