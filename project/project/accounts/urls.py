d8d9ebe264ce474a741945be290e49800e139efc
Nikolay Yordanov <nikolaj2000@abv.bg>
2022-04-26 15:12:36 +0300

    Fixed logout

--- project/project/accounts/urls.py
+++ project/project/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 
-from project.accounts.views import UserLoginView, UserRegisterView, UserDetailsView, ChangeUserPasswordView
+from project.accounts.views import UserLoginView, UserRegisterView, UserDetailsView, ChangeUserPasswordView, LogOutView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login user'),
-    path('logout/', UserLoginView.as_view(), name='logout user'),
+    path('logout/', LogOutView.as_view(), name='logout user'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('<int:pk>/', UserDetailsView.as_view(), name='profile details'),
     path('change-password/', ChangeUserPasswordView.as_view(), name='change password'),
