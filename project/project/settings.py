996a9753715ac6eac2e870d8791de8f95e73bf59
Nikolay Yordanov <nikolaj2000@abv.bg>
2022-04-26 13:03:06 +0300

    Fixed DATABASE exception

--- project/project/settings.py
+++ project/project/settings.py
@@ -63,14 +63,12 @@ DEFAULT_DATABASE_CONFIG = None
 
 if APP_ENVIRONMENT == 'Production':
     DEFAULT_DATABASE_CONFIG = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT', '5432'),
-        }
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME', 'actNews_db'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '1123QwER'),
     }
 else:
     DEFAULT_DATABASE_CONFIG = {
