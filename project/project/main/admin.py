c47b0214021579c873f22107404faf7e5f0a386f
Nikolay Yordanov <nikolaj2000@abv.bg>
2022-04-29 12:08:45 +0300

    Fixed Weather and added to nav bar

--- project/project/main/admin.py
+++ project/project/main/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from project.accounts.models import Profile, ActNewsUser
 from project.main.models import Article
+from project.weather.models import Weather, City
 
 
 @admin.register(Profile)
@@ -18,3 +19,13 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(Article)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+
+@admin.register(Weather)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('temperature',)
+
+
+@admin.register(City)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name',)
